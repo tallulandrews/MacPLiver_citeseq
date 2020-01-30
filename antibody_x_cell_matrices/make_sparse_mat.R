8d11e8048a590d9e463fd1514b751a7bd10b90f4
Geoff Elder <gaelder@uwaterloo.ca>
2020-01-30 12:37:05 -0500

    makefile, antibody annotation v1

--- antibody_x_cell_matrices/make_sparse_mat.R
+++ antibody_x_cell_matrices/make_sparse_mat.R
@@ -3,14 +3,6 @@ args <- commandArgs(trailingOnly=TRUE);
 
 orig_triples <- read.delim(args[1], sep="\t", stringsAsFactors=T)
 
-# get preliminary whitelist
-
-cell_read_count <- as.vector(table(orig_triples[,1]))
-cell_read_count <- cell_read_count[order(cell_read_count, decreasing=T)]
-plot(cell_read_count, log="xy")
-
-write.table(names(cell_read_count)[cell_read_count>200], file="prelim_whitelist.txt", quote=FALSE, row.names=FALSE, col.names=FALSE);
-
 # make matrix
 
 sparse_mat_tuples <- aggregate(orig_triples[,3], by=list(orig_triples[,1], orig_triples[,2]), function(x) {length(unique(x))});
